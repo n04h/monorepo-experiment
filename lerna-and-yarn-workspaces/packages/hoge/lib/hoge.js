fe08d13bac07104dec96531ca5872d15f926f7e4
N04h <contact@n04h.jp>
2021-03-01 14:44:09 +0900

    ✨ 共通パッケージとして追加したaxiosを使えるか確認した

--- lerna-and-yarn-workspaces/packages/hoge/lib/hoge.js
+++ lerna-and-yarn-workspaces/packages/hoge/lib/hoge.js
@@ -1,7 +1,11 @@
-'use strict';
+const axios = require('axios').default;
 
-module.exports = hoge;
-
-function hoge() {
-    // TODO
-}
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
